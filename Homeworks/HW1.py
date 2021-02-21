38a68a1aa47bda97b2d69195841fb90b7fde28d2
Umayyhan <47303162+Umayyhan@users.noreply.github.com>
2021-02-21 14:55:20 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,21 @@
 #Explain your work
+#Generating a 3*3 matrix with 9 random prime numbers.(with #using the for loop)
 
+#***random numbers are generated between 0-50*****
 #Question 1
-for x in range(a):
-	print(a)
+from random import randint
+for i in range(3):
+    for j in range(3):
+        control = False
+        while control is False:
+            num = randint(0,50)
+            
+            if num > 1:
+                for i in range(2, num):
+                    if (num % i) == 0:
+                        break
+                else:
+                    control=True
+                        
+        print(num, end=" ")
+    print("\n")
